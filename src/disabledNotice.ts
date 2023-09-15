c3bff95c9da6c0163316223326cbcbba7870357e
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2023-09-15 18:06:24 +0200

    fix(docs): typo fix

--- src/disabledNotice.ts
+++ src/disabledNotice.ts
@@ -5,7 +5,7 @@ export default function logDisabledNotice() {
   const client = createClient()
 
   log.info(
-    '💎 Unlock semantic search with Embeddings Index APIs — available on Team, Business, and Enterprise plans. ' +
+    '💎 Unlock semantic search with the Embeddings Index API — available on Team, Business, and Enterprise plans. ' +
       `Upgrade now: https://www.sanity.io/manage/project/${
         client.config().projectId
       }/plan?ref=embeddings-cli`,
